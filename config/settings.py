2b45c1f279f2a060f47f20647ff85d789257b5d7
Anjita <156284237+anjitakandel41@users.noreply.github.com>
2026-06-12 20:21:53 +0545

    Remove hardcoded email credentials from settings

--- config/settings.py
+++ config/settings.py
@@ -138,8 +138,8 @@ EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.Email
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER','kandelanjita872@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD','egfcftgfltgaahwj')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL','kandelanjita872@gmail.com')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER','')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD','')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL','')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
